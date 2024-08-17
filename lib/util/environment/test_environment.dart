30d2c7bfaaa0bce1bedec95aff15525520eee5e5
Jaewon Yang <j.yang@draftkings.com>
2024-08-16 20:11:11 -0400

    #5: add run configurations

--- lib/util/environment/test_environment.dart
+++ lib/util/environment/test_environment.dart
@@ -1,1 +1,7 @@
+import "package:currency_converter/util/environment/environment.dart";
+import "package:firebase_core/firebase_core.dart";
 
+class TestEnvironment extends Environment {
+  @override
+  FirebaseOptions? get firebaseOptions => throw UnimplementedError();
+}
