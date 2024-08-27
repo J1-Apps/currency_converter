e67827bfc092b89b6365fc4ed94638c29cb91e9c
Jaewon Yang <j.yang@draftkings.com>
2024-08-27 09:41:04 -0400

    #9: add app test

---
+++ test/app_test.dart
@@ -0,0 +1,15 @@
+import "package:currency_converter/app.dart";
+import "package:currency_converter/ui/screens/home_screen.dart";
+import "package:currency_converter/util/environment/local_environment.dart";
+import "package:flutter_test/flutter_test.dart";
+
+void main() {
+  group("Currency Converter App", () {
+    testWidgets("builds and displays the home page", (tester) async {
+      await LocalEnvironment().configure();
+      await tester.pumpWidget(const CurrencyConverterApp());
+
+      expect(find.byType(HomeScreen), findsOneWidget);
+    });
+  });
+}
